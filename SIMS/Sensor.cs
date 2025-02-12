71625ad8128e51e8cccfcd9867bb680820fb01c4
Torma Alex <torma.alex@students.jedlik.eu>
2025-02-12 11:13:55 +0100

    covert to english

---
+++ SIMS/Sensor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIMS
+{
+    public class Sensor
+    {
+        public int Temperature {  get; set; }
+        public string LightCond { get; set; }
+        public Sensor(int temperature, string lightcond) 
+        {
+            this.Temperature = temperature;
+            this.LightCond = lightcond;
+        }
+
+
+
+
+    }
+}
