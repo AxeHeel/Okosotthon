4f007fcb5e6c08c51afadaf6329edff7a7a9725e
[Alex Torma] <[torma.alex@students.jedlik.eu]>
2025-02-18 20:17:28 +0100

    2 new devices

---
+++ SIMS/Shutter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIMS
+{
+    public class Shutter : Devices
+    {
+        public double Position { get; set; }
+
+        public Shutter(string name) : base(name) { }
+
+        public override void Function(double sensorValue)
+        {
+            Position = sensorValue > 80 ? 0 : 100;
+        }
+    }
+}
---
+++ SIMS/Thermostat.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIMS
+{
+    public class Thermostat : Devices
+    {
+        public double Temperature { get; set; }
+        public bool Heating { get; set; }
+
+        public Thermostat(string name) : base(name) { }
+
+        public override void Function(double sensorValue)
+        {
+            Heating = sensorValue < 20;
+            Temperature = sensorValue;
+        }
+
+        public void ToggleHeating()
+        {
+            Heating = !Heating;
+        }
+    }
+}
