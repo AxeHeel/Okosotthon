dc4495cc3489b70fb7de9771b665d677681ef9bd
[Alex Torma] <[torma.alex@students.jedlik.eu]>
2025-02-18 21:15:56 +0100

    Usher

--- SIMS/User.cs
+++ SIMS/User.cs
@@ -10,5 +10,33 @@ namespace SIMS
     {
         public List<Devices> devices = new List<Devices>();
         public List<Sensor> sensors = new List<Sensor>();
+
+        public void UpdateDevices()
+        {
+            double lightValue = 0;
+            double tempValue = 0;
+            foreach (var sensor in sensors)
+            {
+                if (sensor is lightSensor)
+                {
+                    lightValue = sensor.Value;
+                }
+                if (sensor is tempSensor)
+                {
+                    tempValue = sensor.Value;
+                }
+            }
+            foreach (var device in devices)
+            {
+                if (device is Lamp || device is Shutter)
+                {
+                    device.Function(lightValue);
+                }
+                else if (device is Thermostat)
+                {
+                    device.Function(tempValue);
+                }
+            }
+        }
     }
 }
